afeee161817935e5bb643ba14e5b6b72d8c6f533
maksimgm <maksimmamrikov@gmail.com>
2015-11-15 20:16:50 -0800

    add upvotes and downvotes

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ app.controller("NewPostController", function($scope) { // What's missing?
   $scope.posts = {}; // Store todo items here. What is the data type?
   $scope.posts.data = [];
   $scope.posts.newPost = {};
-  
+  $scope.rightNow = new Date();
+  $scope.newComment = '';
+
   $scope.addPost = function(newPost) {
+    newPost.votes = 0;
+    newPost.comments = [];
+    newPost.showComment = false;
+    // newPost.index = $scope.posts.data.length;
     $scope.posts.data.push(newPost);
     $scope.posts.newPost = {};
   };
@@ -21,6 +27,24 @@ app.controller("NewPostController", function($scope) { // What's missing?
     if ($scope.posts.new) {
       $scope.posts.new = false;
     }
-  }
+  };
+
+  $scope.upvotes = function(index){
+    $scope.posts.data[index].votes++;
+  };
+
+  $scope.downvotes = function(index){
+    $scope.posts.data[index].votes--;
+  };
+
+  $scope.addComment = function(index, newComment){
+    $scope.posts.data[index].comments.push(newComment);
+  };
+
+  $scope.commentForm = function(index){
+    // newPost.showComment = true;
+    $scope.posts.data[index].showComment = true;
+  };
+
 });
 
