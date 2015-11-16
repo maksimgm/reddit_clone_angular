31918bbfc12bf9b6e46464749b83b949dc1317ac
maksimgm <maksimmamrikov@gmail.com>
2015-11-16 10:56:24 -0800

    add comment functionality to form

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var app = angular.module('redditClone', []); // What's missing? Hint, it's more than one thing.
 
 app.controller("NewPostController", function($scope) { // What's missing?
-  $scope.posts = {}; // Store todo items here. What is the data type?
+  $scope.posts = {}; // Store posts here. What is the data type?
   $scope.posts.data = [];
   $scope.posts.newPost = {};
   $scope.rightNow = new Date();
-  $scope.newComment = '';
+  $scope.comment = {};
 
   $scope.addPost = function(newPost) {
     newPost.votes = 0;
     newPost.comments = [];
     newPost.showComment = false;
+    newPost.allComments = false;
     // newPost.index = $scope.posts.data.length;
     $scope.posts.data.push(newPost);
     $scope.posts.newPost = {};
@@ -37,14 +38,23 @@ app.controller("NewPostController", function($scope) { // What's missing?
     $scope.posts.data[index].votes--;
   };
 
-  $scope.addComment = function(index, newComment){
-    $scope.posts.data[index].comments.push(newComment);
+  $scope.addComment = function(data, comment){
+    data.comments.push(comment);
+    data.showComment = false;
+    data.allComments = false;
+    $scope.comment = {};
   };
 
   $scope.commentForm = function(index){
     // newPost.showComment = true;
     $scope.posts.data[index].showComment = true;
   };
+
+  $scope.showAllComments = function(data){
+    data.allComments = !data.allComments;
+  };
+
+
 
 });
 
